d58c97639c7d155f81a8f29755ead43c36fe44ed
ruxxzebre <paullaboratory@gmail.com>
2025-09-19 00:48:01 +0300

    tab stop

--- .history/init_20250331155550.lua
+++ .history/init_20250331155550.lua
@@ -1,7 +1,3 @@
-vim.opt.tabstop = 8 -- Always 8 (see :h tabstop)
-vim.opt.softtabstop = 4 -- What you expecting
-vim.opt.shiftwidth = 4 -- What you expecting
-
 --[[
 
 =====================================================================
